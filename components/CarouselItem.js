0d148b12cd0f8d5bfda408bbc489dd1c763ebecc
levis-creator <levisluiz@zoho.com>
2023-04-06 21:21:22 +0300

    v1.0.2

--- components/CarouselItem.js
+++ components/CarouselItem.js
@@ -6,7 +6,8 @@ const CarouselItem = ({img, middleText}) => {
     return (
         <div className='h-80 sm:h-96 m-0'>
             <Image 
-            src={img} className="rounded-full object-cover h-64 sm:h-72 md:h-80 w-64 sm:w-72 md:w-80 mx-auto relative"/>
+            src={img} className="rounded-full object-cover h-64 sm:h-72 md:h-80 w-64 sm:w-72 md:w-80 mx-auto relative"
+            alt={middleText}/>
             <div className={`${styles.carouselText} 
             ${styles.carouselTextPosition} 
             ${styles.carouselTextTransform}`}>
